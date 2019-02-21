6db8af6c43af7bfb62200686b9f732a06d1131d5
SerJaime <1390084914@qq.com>
2019-02-21 10:34:39 +0800

    init

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const routes = require('./routes/index');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+// the cores config
+app.all('*', function(req, res, next) {
+  let origin = req.headers.origin
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  // res.header("Cache-Control", "no-store");
+
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+routes(app);
+
+app.listen(8080, function () {
+  console.log('server listening on port 8080');
+})
---
+++ server/routes/index.js
@@ -0,0 +1,104 @@
+const { resolve } = require('path');
+const axios = require('axios');
+const BASE_URL = 'https://news-at.zhihu.com';
+
+axios.defaults.baseURL = BASE_URL;
+axios.interceptors.response.use(res => {
+  return res.data;
+}, (err) => {
+  return Promise.reject(err);
+});
+
+module.exports = function (app) {
+  app.get('/', (req, res) => {
+    res.sendFile(resolve(__dirname, '../../dist/index.html'));
+  })
+
+  app.get('/api/news/latest', async (req, res) => {
+    try {
+      const result = await axios.get('/api/4/news/latest');
+      res.json(result);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  })
+
+  app.get('/api/news/hot', async (req, res) => {
+    try {
+      const result = await axios.get('/api/3/news/hot');
+      res.json(result);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  })
+
+  app.get('/api/news/before/:day', async (req, res) => {
+    const today = req.params.day;
+    const nextDay = parseInt(today) + 1;
+    try {
+      const result = await axios.get(`/api/4/news/before/${nextDay}`);
+      res.json(result);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  })
+
+  app.get('/api/news/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+      const result = await axios.get(`/api/4/news/${id}`);
+      res.json(result);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  })
+
+  app.get('/api/news/:id/extra-info', async (req, res) => {
+    const id = req.params.id;
+    try {
+      const result = await axios.get(`/api/4/story-extra/${id}`);
+      res.json(result);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  })
+
+  app.get('/api/news/:id/short-comments', async (req, res) => {
+    const id = req.params.id;
+    try {
+      const result = await axios.get(`/api/4/story/${id}/short-comments`);
+      res.json(result);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  })
+
+  app.get('/api/news/:id/long-comments', async (req, res) => {
+    const id = req.params.id;
+    try {
+      const result = await axios.get(`/api/4/story/${id}/long-comments`);
+      res.json(result);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  })
+
+  app.get('/api/sections', async (req, res) => {
+    try {
+      const result = await axios.get('/api/3/sections');
+      res.json(result);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  })
+
+  app.get('/api/section/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+      const result = await axios.get(`/api/3/section/${id}`);
+      res.json(result);
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  })
+}
